fde990285a7a0353ce5fe26d1e28f61b7d104564
RaskiTech <raskitech@gmail.com>
2020-12-11 20:32:09 +0200

    Getting git working

--- premake5.lua
+++ premake5.lua
@@ -13,7 +13,7 @@ project "GLFW"
     "src/init.c",
     "src/input.c",
     "src/monitor.c",
-    "src/vilkan.c",
+    "src/vulkan.c",
     "src/window.c"
   }
   
@@ -27,7 +27,7 @@ project "GLFW"
     "src/win32_joystick.c",
     "src/win32_monitor.c",
     "src/win32_time.c",
-    "src/thread.c",
+    "src/win32_thread.c",
     "src/win32_window.c",
     "src/wgl_context.c",
     "src/egl_context.c",
